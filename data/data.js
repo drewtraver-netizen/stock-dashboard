ee19ce81fd5c0384f98451459e2e9822ab0b45fe
DrewTraver <drew.traver@icloud.com>
2026-03-15 14:42:23 -0700

    Update dashboard data (DailyChartData)

--- data/data.js
+++ data/data.js
@@ -778,7 +778,7 @@ window.DASHBOARD_DATA = {
       "Signal": 0.25
     }
   ],
-  "modelScore": null,
+  "modelScore": 0.25,
   "spQuote": 6632.19,
   "ytdReturn": 0.03775960213634666,
   "portfolioWeights": [
@@ -858,6 +858,6 @@ window.DASHBOARD_DATA = {
     ],
     "myReturn": 2.8712647919260093e-06
   },
-  "generatedAt": "2026-03-14T00:45:51.029627+00:00",
+  "generatedAt": "2026-03-15T21:42:23.424267+00:00",
   "rowCount": 154
 };
