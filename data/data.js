d9c9b3212d80b86132710a2dc374692b5a47db3c
DrewTraver <drew.traver@icloud.com>
2026-03-30 13:31:30 -0700

    Update dashboard data (DailyChartData)

--- data/data.js
+++ data/data.js
@@ -835,35 +835,35 @@ window.DASHBOARD_DATA = {
   ],
   "modelScore": 0.0625,
   "spQuote": 6343.72,
-  "ytdReturn": 0.017169615790598947,
+  "ytdReturn": 0.017081665043017447,
   "portfolioWeights": [
     {
       "symbol": "QQQ",
-      "weight": 0.09345097517213274
+      "weight": 0.0934591883849543
     },
     {
       "symbol": "DIA",
-      "weight": 0.12294587426664927
+      "weight": 0.12295667972511586
     },
     {
       "symbol": "GLD",
-      "weight": 0.12145630475795648
+      "weight": 0.12146697930124144
     },
     {
       "symbol": "XLE",
-      "weight": 0.12962330532872446
+      "weight": 0.1296346976527889
     },
     {
       "symbol": "BTAL",
-      "weight": 0.08407206469908231
+      "weight": 0.08399156568825894
     },
     {
       "symbol": "BITO",
-      "weight": 0.0908906271888318
+      "weight": 0.09089861537796746
     },
     {
       "symbol": "CASH",
-      "weight": 0.357560848586623
+      "weight": 0.3575922738696732
     }
   ],
   "indexReturns": [
@@ -911,8 +911,8 @@ window.DASHBOARD_DATA = {
         "return": -0.00019290000000000003
       }
     ],
-    "myReturn": 0.0008026901760411764
+    "myReturn": 0.0007147394284596764
   },
-  "generatedAt": "2026-03-30T20:26:33.015735+00:00",
+  "generatedAt": "2026-03-30T20:31:30.258634+00:00",
   "rowCount": 165
 };
